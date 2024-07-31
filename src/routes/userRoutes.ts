152736995d71766ea74c3affadc31e0d34649364
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2024-07-31 14:46:06 +0100

    add userRoutes for registry and login

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Router } from "express";
+import { createUser, loginUser } from "../controllers/userController";
 
 const router = Router();
-const prisma = new PrismaClient();
 
+// Route to create a new user
+router.post("/register", createUser);
+
+// Route to login a user
+router.post("/login", loginUser);
 
 export default router;
